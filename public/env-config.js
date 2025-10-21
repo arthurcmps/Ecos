d41ff07edb0fa39448878c6beb79f7415449d7dd
arthur.felipe1993 <arthur.felipe1993@gmail.com>
2025-10-21 06:32:32 +0000

    Reiniciando

--- public/env-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAy0Q4pK0oigiRUVMRFEm8QQsU5zvJeqHI",
-  authDomain: "ecos-4f84d.firebaseapp.com",
-  databaseURL: "https://ecos-4f84d-default-rtdb.firebaseio.com",
-  projectId: "ecos-4f84d",
-  storageBucket: "ecos-4f84d.firebasestorage.app",
-  messagingSenderId: "145323796661",
-  appId: "1:145323796661:web:e53bd8a7cb2fa37204b6fc"
-};
--- public/firebase-config.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { firebaseConfig } from "./env-config.js"; // Importa a configuração
-
-// Inicializa e exporta as instâncias
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
